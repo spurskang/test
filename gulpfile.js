b37f675c068aadb3d47070dfac8134d77714181e
spurskang <aj4hjpwji4y9@gmail.com>
2020-05-28 13:36:33 +0800

    建立開發環境

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,41 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var concat = require('gulp-concat');
-var sass =  require('gulp-sass');
+var sass = require('gulp-sass');
+var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-var fileinclude = require('gulp-file-include');
 
-gulp.task('move',function(){
-  //do
-  return gulp.src('*.html') //來源
-  .pipe(gulp.dest('dest/'))     //目的地
-});
 
+
+// sass 轉譯
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss')  //來源
-    .pipe(sass.sync().on('error', sass.logError)) //轉譯
-    .pipe(gulp.dest('./css')); //目的地
-});
-
-gulp.task('default',function(){
-  browserSync.init({
-    server:{
-      baseDir: "./dest",
-      index: "index.html"
-    }
+    return gulp.src('./dev/sass/*.scss')//來源
+      .pipe(sass().on('error', sass.logError)) //sass轉譯
+      .pipe(gulp.dest('./dest/css')); //目的地
   });
-  gulp.watch('./sass/*.scss',['concat']).on('change',reload);
-  gulp.watch(['./*.html', './**/*.html'],['fileinclude']).on('change',reload);
+
+
+  // html 樣板
+gulp.task('fileinclude', function () {
+    gulp.src(['dev/*.html'])
+      .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+      }))
+      .pipe(gulp.dest('./dest'));
+  });
+
+
+
+//同步
+gulp.task('default', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dest",
+            index : "index.html"
+        }
+    });
+    gulp.watch('./dev/sass/*.scss' ,['sass']).on('change',reload);
+    gulp.watch(['./dev/*.html' ,'./dev/**/*.html'] ,['fileinclude']).on('change',reload);
 });
